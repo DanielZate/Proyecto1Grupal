1471d85446115d749f17ae5ccd73506422083f35
Daniel Alzate <dsalzate91@misena.edu.co>
2021-06-02 23:19:30 -0500

    Carga del proyecto

---
+++ src/test/java/com/ProyectoFinal/ProyectoOrnamentacion/ProyectoOrnamentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinal.ProyectoOrnamentacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOrnamentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
